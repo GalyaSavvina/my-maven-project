c701199cc3f0c8bd7b4ec22a35a03c0452039ce5
savvi <savvina02@inbox.ru>
2023-10-19 16:25:49 +0300

    19.10

---
+++ src/main/java/com/example/ListContainer.java
@@ -0,0 +1,124 @@
+package com.example;
+
+/**
+ * Класс `ListContainer` представляет собой контейнер для хранения элементов.
+ *
+ * @param <T> Тип элементов, которые могут быть добавлены в контейнер.
+ */
+public class ListContainer<T> {
+    /**
+     * Внутренний класс `Node` представляет элемент контейнера.
+     *
+     * @param <T> Тип данных, хранимых в узле.
+     */
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        /**
+         * Создает новый узел с указанными данными.
+         *
+         * @param data Данные, которые будут храниться в узле.
+         */
+        Node(T data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    /**
+     * Голова контейнера-первый элемент
+     */
+    private Node<T> head;
+
+    /**
+     * Количество элементов в контейнере
+     */
+    private int size;
+
+    /**
+     * Создает новый контейнер.
+     */
+    public ListContainer() {
+        head = null;
+        size = 0;
+    }
+
+    /**
+     * Добавляет элемент в контейнер.
+     *
+     * @param item Элемент, который необходимо добавить.
+     */
+    public void add(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    /**
+     * Извлекает элемент по индексу.
+     *
+     * @param index Индекс элемента для извлечения.
+     * @return Элемент, находящийся по указанному индексу, или `null`, если индекс некорректен.
+     */
+    public T get(int index) {
+            if (index < 0 || index >= size || head == null) {
+                return null;
+            }
+            Node<T> current = head;
+            for (int i = 0; i < index; i++) {
+                if (current.next == null) {
+                    return null;
+                }
+                current = current.next;
+            }
+            return current.data;
+    }
+
+    /**
+     * Удаляет элемент по индексу.
+     *
+     * @param index Индекс элемента для удаления.
+     */
+    public void remove(int index) {
+            if (index < 0 || index >= size) {
+                return; // Проверка на корректность индекса
+            }
+
+            if (index == 0) {
+                // Удаление первого элемента
+                head = head.next;
+            } else {
+                // Удаление элемента по индексу
+                Node<T> current = head;
+                for (int i = 0; i < index - 1; i++) {
+                    current = current.next;
+                }
+                current.next = current.next.next;
+            }
+            size--;
+    }
+
+    /**
+     * Возвращает количество элементов в контейнере.
+     *
+     * @return Количество элементов в контейнере
+     */
+    public int size() {
+
+        return size;
+    }
+}
+
+
+
+
+
---
+++ src/test/java/com/example/ListContainerTest.java
@@ -0,0 +1,70 @@
+import com.example.ListContainer;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class ListContainerTest {
+    /**
+     * Тест метода add и size
+     */
+    @Test
+    public void testAddAndSize() {
+        ListContainer<String> container = new ListContainer<>();
+        assertEquals(0, container.size());
+
+        container.add("Item 1");
+        assertEquals(1, container.size());
+
+        container.add("Item 2");
+        assertEquals(2, container.size());
+    }
+    /**
+     * Тест метода get
+     */
+    @Test
+    public void testGet() {
+        ListContainer<String> container = new ListContainer<>();
+        container.add("Item 1");
+        container.add("Item 2");
+
+        assertEquals("Item 1", container.get(0));
+        assertEquals("Item 2", container.get(1));
+        assertNull(container.get(2));
+    }
+    /**
+     * Тест метода remove
+     */
+    @Test
+    public void testRemove() {
+        ListContainer<String> container = new ListContainer<>();
+        container.add("Item 1");
+        container.add("Item 2");
+        container.add("Item 3");
+
+        // Попробуйте удалить элемент по индексу 0
+        container.remove(0);
+        assertEquals(2, container.size()); // Размер контейнера должен уменьшиться на 1
+        assertEquals("Item 2", container.get(0)); // Проверяем, что первый элемент теперь "Item 2"
+
+        // Попробуйте удалить элемент по индексу 1
+        container.remove(1);
+        assertEquals(1, container.size()); // Размер контейнера должен уменьшиться ещё на 1
+        assertNull(container.get(1)); // Проверяем, что второй элемент теперь null
+    }
+    /**
+     * Тест метода add и remove для элементов со значением null
+     */
+    @Test
+    public void testAddAndRemoveNull() {
+        ListContainer<String> container = new ListContainer<>();
+        container.add("Item 1");
+        container.add(null);
+        container.add("Item 2");
+
+        assertEquals(3, container.size());
+
+        container.remove(1);
+        assertEquals(2, container.size());
+
+        // После удаления элемента, его место займет следующий элемент
+        assertEquals("Item 2", container.get(1));
+    }
+}
